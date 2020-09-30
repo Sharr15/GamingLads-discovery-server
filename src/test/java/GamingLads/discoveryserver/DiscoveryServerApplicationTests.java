e171237bf781d5980753d6930ef937f59b952c27
Sharr15 <fogsliezeta@hotmail.com>
2020-09-30 14:43:28 +0200

    initial commit

---
+++ src/test/java/GamingLads/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package GamingLads.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
